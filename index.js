23bb755eb2f436afa1aee44fb8d89fe44339718c
Michael Schwartz <mykesx@gmail.com>
2015-02-27 11:04:05 -0800

    WebSocket, XHR, XHR-Streaming done

--- index.js
+++ index.js
@@ -4,6 +4,14 @@
 
 /*global require, exports */
 
+//exports.Session = require('lib/Session');
+var Session = require('lib/Session');
+
+//console.log('x');
+//console.dir(Session);
+//console.log('y');
+
 decaf.extend(exports, {
-    SockJS: require('lib/jolt-sockjs').SockJS
+    SockJS  : require('lib/jolt-sockjs').SockJS,
+    Session : require('lib/Session')
 });
